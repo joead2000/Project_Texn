a1d8445a192c0465c9e4f891a5bee92db4c1f4f4
joead2000 <45120866+joead2000@users.noreply.github.com>
2021-11-16 18:09:07 +0200

    Adding Actions for Buttons
    Buttons forward,rewind,next ,prev
    are now working
    Also added Visualizer

--- Petify_v2/app/src/main/java/com/petify_v2/MainActivity.java
+++ Petify_v2/app/src/main/java/com/petify_v2/MainActivity.java
@@ -15,10 +15,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.io.File;
@@ -46,20 +48,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void runtimePermission() {
-        Dexter.withContext(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE).withListener(new PermissionListener() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO).withListener(new MultiplePermissionsListener() {
             @Override
-            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
                 displaySongs();
             }
 
             @Override
-            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
 
-            }
-
-            @Override
-            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                permissionToken.cancelPermissionRequest();
             }
         }).check();
 
