b8ae0b088dd07cd993e6a3180b478e2414313167
alextitonis <alexandrostitonis@gmail.com>
2021-11-24 01:16:34 +0200

    fixed some merge issues, fixed db connection

--- JSServer/src/postgres.ts
+++ JSServer/src/postgres.ts
@@ -11,39 +11,44 @@ export class postgres {
     async connect() {
         this.client = new Client();
         this.client.connect();
-        const res = await postgres.getInstance.client.query('SELECT NOW()')
+        const res = await this.client.query('SELECT NOW()')
     }
 
     async login(username: string, password: string, errorCallback: Function, callback: Function) {
         const query = "SELECT * FROM users WHERE username=$1 AND password=$2"
         const values = [username, password]
 
-        postgres.getInstance.client.query(query, values, (err, res) => {
+        await postgres.getInstance.client.query(query, values, (err, res) => {
             if (err) {
                 console.log(err)
+                if (errorCallback)
                 errorCallback()
             } else {
+                if (callback)
                 callback()
             }
         });
     }
     async register(email: string, username: string, password: string, errorCallback: Function, callback: Function) {
-        const query = "SELECT * FROM users WHERE email=$1 OR passowrd=$2"
+        const query = "SELECT * FROM users WHERE email=$1 OR password=$2"
         const values = [email, username]
 
-        postgres.getInstance.client(query, values, (err, res) => {
+        await postgres.getInstance.client.query(query, values, (err, res) => {
             if (err) {
                 console.log(err)
+                if (errorCallback)
                 errorCallback()
             } else {
                 const query2 = "INSERT INTO users(username, email, password) VALUES($1, $2, $3)"
                 const values2 = [email, username, password]
 
-                postgres.getInstance.client(query2, values2, (err, res) => {
+                postgres.getInstance.client.query(query2, values2, (err, res) => {
                     if (err) {
                         console.log(err)
+                        if (errorCallback)
                         errorCallback()
                     } else {
+                        if (callback)
                         callback()
                     }
                 })
