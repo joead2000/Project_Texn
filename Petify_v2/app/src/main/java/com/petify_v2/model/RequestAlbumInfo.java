5c4ac43fa26a86891ef24faf33b695a87c4cf5e7
alextitonis <alexandrostitonis@gmail.com>
2021-12-13 22:10:17 +0200

    made the album requests to run through th eserver

--- Petify_v2/app/src/main/java/com/petify_v2/model/RequestAlbumInfo.java
+++ Petify_v2/app/src/main/java/com/petify_v2/model/RequestAlbumInfo.java
@@ -1,18 +1,12 @@
 package com.petify_v2.model;
 
-
 import android.content.Context;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.petify_v2.R;
+import com.petify_v2.model.Album;
 import com.petify_v2.model.IVolleyCallBackMessage;
 import com.petify_v2.model.RequestSingleton;
 
@@ -29,30 +23,23 @@ import java.util.Map;
 public class RequestAlbumInfo {
 
     public static void textViewAlbum(String artist, Context context, IVolleyCallBackMessage volleyCallBackMessage) {
-        String URL = "https://theaudiodb.com/api/v1/json/2/discography.php?s="+artist;
+        String URL = "https://7b9f-2-85-54-248.ngrok.io/artistAlbums";
         System.out.println(URL);
 
-
         JsonObjectRequest request = new JsonObjectRequest
-                (Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+                (Request.Method.POST, URL, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-
                             List<Album> albums = new ArrayList<>();
-                            for (int i = 0; i < response.getJSONArray("album").length(); i++) {
-                                String title = response.getJSONArray("album").getJSONObject(i).getString("strAlbum");
-                                String year = response.getJSONArray("album").getJSONObject(i).getString("intYearReleased");
+                            for (int i = 0; i < response.getJSONArray("artist").length(); i++) {
+                                String title = response.getJSONArray("artist").getJSONObject(i).getString("strAlbum");
+                                String year = response.getJSONArray("artist").getJSONObject(i).getString("intYearReleased");
                                 albums.add(new Album(title,year));
 
 
                             }
                             volleyCallBackMessage.onSuccessInfo(albums);
-
-
-
-
-
                         } catch (JSONException e) {
                             volleyCallBackMessage.onSuccess("JSON error");
                         }
@@ -62,7 +49,28 @@ public class RequestAlbumInfo {
                     public void onErrorResponse(VolleyError error) {
                         volleyCallBackMessage.onWarning("Connection Error");
                     }
-                });
+                }){
+
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+
+            @Override
+            public byte[] getBody() {
+                try {
+                    JSONObject body = new JSONObject();
+                    body.put("artist",artist);
+                    return body.toString().getBytes(StandardCharsets.UTF_8);
+                } catch (Exception exception) {
+                    return null;
+                }
+            }
+
+        };
 
         RequestSingleton.getInstance(context).addToRequestQueue(request);
     }
