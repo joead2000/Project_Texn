de200bd3a94cb90265e669adeda2ffe969b14338
alextitonis <alexandrostitonis@gmail.com>
2021-12-22 12:34:36 +0200

    fixed an issue with password var names

--- JSServer/src/tests/postgres.spec.ts
+++ JSServer/src/tests/postgres.spec.ts
@@ -4,10 +4,10 @@ const p = new postgres();
 p.connect();
 const email = "test@gmail.com"
 const username = "test"
-const password = "test"
+const _password = "test"
 
 test('test the register, login and forgot password functions', async() => {
-    await p.register(email, username, password, () => {}, () => {});
-    await p.login(username, password, () => {}, () => { expect(true).toBe(true) });
-    await p.forgotPassword(email, (password) => { expect(password).toBe(password) }, () => {})
+    await p.register(email, username, _password, () => {}, () => {});
+    await p.login(username, _password, () => {}, () => { expect(true).toBe(true) });
+    await p.forgotPassword(email, (password) => { expect(password).toBe(_password) }, () => {})
 })
