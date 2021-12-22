2987037da9eebbbbc93af1a13b72bd1dc8267dcc
alextitonis <alexandrostitonis@gmail.com>
2021-12-22 12:27:12 +0200

    updated tests

--- JSServer/src/tests/audioDB.spec.ts
+++ JSServer/src/tests/audioDB.spec.ts
@@ -5,9 +5,15 @@ const adb = new audioDB();
 new cacheManager();
 
 test('biography length should not be 0', async() => {
-  await adb.requestBiography('drake', (artist) => expect(artist.length).not.toBe(0));
+  await adb.requestBiography('drake', (artist) => {
+    expect(artist.length).not.toBe(0)
+    expect(artist).not.toBe('no artist found!')
+  });
 })
 
 test('album length should not be 0', async() => {
-  await adb.requestAlbums('drake', (artist) => expect(artist.length).not.toBe(0))
+  await adb.requestAlbums('drake', (artist) => {
+    expect(artist.length).not.toBe(0)
+    expect(artist).not.toBe('no albums found!')
+  })
 })
