d8ffe9b3747859f5c1c79c515f07b0dd215069cb
joead2000 <45120866+joead2000@users.noreply.github.com>
2022-01-19 17:43:48 +0200

    Tests

--- Petify_v2/app/src/androidTest/java/com/petify_v2/view/youtubeSearchFailedTest.java
+++ Petify_v2/app/src/androidTest/java/com/petify_v2/view/youtubeSearchFailedTest.java
@@ -7,16 +7,21 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import com.petify_v2.R;
 
+import com.petify_v2.R;
 @RunWith(AndroidJUnit4.class)
 
-public class youtubeSearchTest {
+public class youtubeSearchFailedTest {
 
 
     @Rule
@@ -24,11 +29,11 @@ public class youtubeSearchTest {
             = new ActivityScenarioRule<>(LoginActivity.class);
 
     @Test
-    public void youtubeSearchTest()
+    public void youtubeSearchFailedTest()
     {
         onView(withId(R.id.btnskip)).perform(click());
         onView(withId(R.id.openYoutubeFormbtn)).perform(click());
-        onView(withId(R.id.videoTitle)).perform(typeText("The Prince Karma"), closeSoftKeyboard());
+        onView(withId(R.id.videoTitle)).perform(typeText("     "), closeSoftKeyboard());
         onView(withId(R.id.searchButton)).perform(click());
 
 
