71d6bc4d9bc24089c45322bb2897b15c2b03f271
joead2000 <45120866+joead2000@users.noreply.github.com>
2022-01-26 16:57:50 +0200

    Tests Added

---
+++ Petify_v2/app/src/androidTest/java/com/petify_v2/view/LoginFailedTest.java
@@ -0,0 +1,37 @@
+package com.petify_v2.view;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.petify_v2.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+
+public class LoginFailedTest {
+
+
+    @Rule
+    public ActivityScenarioRule<LoginActivity> activityRule
+            = new ActivityScenarioRule<>(LoginActivity.class);
+    private final LoginActivity loginActivity = null;
+
+    @Test
+    public void test_Login() {
+        onView(withId(R.id.usernameEditText)).perform(typeText("anastasia"));
+
+        onView(withId(R.id.passwordEditText)).perform(typeText("password"));
+
+        onView(withId(R.id.btnConnect)).perform(click());
+    }
+
+}
